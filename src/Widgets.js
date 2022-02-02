1ac5d810be0a62cde249d86c5c31ae49d2e826a8
Rontu22 <rontubarhoi01@gmail.com>
2022-02-02 15:57:19 +0530

    Left widgets are working fine

---
+++ src/Widgets.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Widgets.css";
+import InfoIcon from "@material-ui/icons/Info";
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
+function Widgets() {
+  const newsArticle = (heading, subtitle) => (
+    <div className="widgets_article">
+      <div className="widgets_articleLeft">
+        <FiberManualRecordIcon />
+      </div>
+      <div className="widgets_articleRight">
+        <h4>{heading}</h4>
+        <p>{subtitle}</p>
+      </div>
+    </div>
+  );
+
+  return (
+    <div className="widgets">
+      <div className="widgets_header">
+        <h2>LinkedIn News</h2>
+        <InfoIcon />
+      </div>
+      {newsArticle(
+        "Google to launch new search",
+        "Google is launching a new search engine"
+      )}
+      ,
+      {newsArticle(
+        "Coronavirus: US cases hit 1.5 million",
+        "Is Redux too good"
+      )}
+    </div>
+  );
+}
+
+export default Widgets;
